4074611294e854a3b9a74853b4f4ab49e45d756d
Nisb <nathalia.ig.bomfim@gmail.com>
2021-10-29 13:38:02 -0300

    Upando arquivos de estudo.

---
+++ principal/naty-springboot/src/main/java/br/com/devdojo/SpringBootEssentialsApplication.java
@@ -0,0 +1,11 @@
+package br.com.devdojo;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootEssentialsApplication {//Essa será a nossa classe responsável por iniciar o Spring Boot e ele vai startar o servidor p vc
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEssentialsApplication.class, args);
+    }
+
+}
